d094098506753dd7842c0f17ea833303a2cf1af7
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-06 13:45:40 +0530

    frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Textondashboard from "@/component/Textondashboard";
 import TopBar from "@/component/TopBar";
 export default function Home() {
   return (
 <div className="min-h-screen bg-black-100 text-white flex flex-col overflow-hidden">
       <TopBar />
       <div className="flex flex-col justify-center items-center flex-1 p-4">
+      <Textondashboard />
       </div>
     </div>
   );
---
+++ component/Textondashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Textondashboard = () => {
+  return (
+    <div className="p-10 text-blue-100 text-3xl text-center">
+      <div className="p-4">
+        Welcome to ShortenIt
+      </div>
+      <div className="text-lg text-white">
+        Easily shorten URLs and manage them effectively.
+      </div>
+    </div>
+  );
+};
+
+export default Textondashboard;
