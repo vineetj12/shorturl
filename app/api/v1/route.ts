861c9eeef146b656d246de052d4ffdcfde8816fb
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-06 12:33:03 +0530

    helllo

--- app/api/v1/route.ts
+++ app/api/v1/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { PrismaClient } from "@/app/generated/prisma";
 import crypto from "crypto";
 import { NextRequest, NextResponse } from "next/server";
