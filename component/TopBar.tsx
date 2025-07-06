f19a1aa67982710632e3f4c83f76685debf9b4f3
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-06 13:42:37 +0530

    frontend

---
+++ component/TopBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Image from 'next/image';
+
+const TopBar = () => {
+  return (
+    <header className="text-blue-100 flex justify-between items-center p-2">
+      <div className="flex items-center gap-2">
+        <div className="bg-blue-100 rounded-full p-1">
+          <Image
+            src="/chain-svgrepo-com.svg"
+            alt="Chain Icon"
+            width={30}
+            height={30}
+          />
+        </div>
+        <span className="text-xl font-semibold">ShortenIt</span>
+      </div>
+
+      <div className="flex space-x-4">
+        <button className="shadow-[inset_0_0_0_2px_#616467] text-blue-100 px-4 py-1.5 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white transition duration-200">
+          Signup
+        </button>
+        <button className="shadow-[inset_0_0_0_2px_#616467] text-blue-100 px-4 py-1.5 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white transition duration-200">
+          Create Account
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
