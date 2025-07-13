4c8e6428cffc6faf229399dbe648bd792097714e
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-13 16:49:55 +0530

    fix the page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,38 @@
 import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
-const handler=NextAuth({
-         providers:[
-            CredentialsProvider({
-                name:"Email",
-                credentials:{
-                    username:{label:'email',type:'text',placeholder:'email'},
-                    password:{label:'password',type:'text',placeholder:'password'}
-                },
-               async authorize(credentials:any) {
-                  return   {
-                    id:"user1",
-                    name:credentials.username
-                  }
-               }
-            }),
-            GitHubProvider({
-             clientId: process.env.GITHUB_ID||"",
-             clientSecret: process.env.GITHUB_SECRET||""
-             }),
-         ],  
-         secret:process.env.NEXTAUTH_SECRET||"",
-         pages:{
-            signIn:"/signin"
-         }
-});
 
-export {handler as GET , handler as POST};
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        username: { label: "email", type: "text", placeholder: "email" },
+        password: { label: "password", type: "text", placeholder: "password" },
+      },
+      async authorize(
+        credentials: Record<"username" | "password", string> | undefined
+      ) {
+        if (!credentials) return null;
+
+        return {
+          id: "user1",
+          name: credentials.username,
+        };
+      },
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET || "",
+  pages: {
+    signIn: "/signin",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
