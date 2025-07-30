0b602292dfa7ae21aa2641180956a27dbcb45ce3
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-30 23:47:50 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import ShortBox from "@/component/ShortBox";
 import Textondashboard from "@/component/Textondashboard";
 import TopBar from "@/component/TopBar";
 import { useSession } from "next-auth/react";
+
 export default function Home() {
-  const session=useSession();
+  const { status } = useSession();
+
   return (
-<div className="min-h-screen bg-black-100 text-white flex flex-col overflow-hidden">
+    <div className="min-h-screen bg-black-100 text-white flex flex-col overflow-hidden">
       <TopBar />
       <div className="flex flex-col justify-center items-center flex-1 p-4">
-      <Textondashboard />
-      if(JSON.stringify(session).status!='unauthenticated'){
-        <ShortBox />
-      }
+        <Textondashboard />
+        {status !== "unauthenticated" && <ShortBox />}
       </div>
     </div>
   );
