38b2f3b6e481b558a59664fe76eb1f034554bf04
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-30 23:44:05 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center flex-1 p-4">
       <Textondashboard />
       if(JSON.stringify(session).status!='unauthenticated'){
-      <ShortBox />
+        <ShortBox />
       }
       </div>
     </div>
