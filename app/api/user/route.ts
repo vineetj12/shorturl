70ac0731f1335d9b2ec50bc2245a2d2565faa7fd
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-31 12:16:58 +0530

    add

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -16,8 +16,9 @@ export async function GET(req: Request) {
       where: { username },
     });
 
-    return NextResponse.json(data);
+    return NextResponse.json({ data });
   } catch (error) {
+    console.error("Error fetching user:", error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
